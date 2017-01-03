101813309133de1a829f70acc2ef4fec86eb610f
CHEN Mingyi <imcmy@outlook.com>
2017-01-03 15:35:29 +0800

    [Add]Looping
    Looping until all course selected

--- evaluate.py
+++ evaluate.py
@@ -1,8 +1,8 @@
 # @file evaluate.py
 # @brief enroll & evaluate
 # @author scusjs@foxmail.com, my@imcmy.me
-# @version 0.2.00
-# @date 2016-9-3
+# @version 0.2.01
+# @date 2017-1-3
 
 import requests
 from configparser import RawConfigParser
@@ -89,24 +89,32 @@ class UCASEvaluate:
                 print(response.text)
                 exit()
 
-            for eachCourse in self.coursesId:
-                if eachCourse in response.text:
-                    print("Course " + eachCourse + " has been selected.")
-                    continue
-                if (eachCourse in self.enrollCount and
-                        self.enrollCount[eachCourse] == 1):
-                    continue
-                self.enrollCount[eachCourse] = 1
-                result = self.__enrollCourse(eachCourse,
-                                             self.coursesId[eachCourse])
-                if result:
-                    self.enrollCount[eachCourse] = 0
+            while True:
+                for eachCourse in self.coursesId:
+                    if eachCourse in response.text:
+                        print("Course " + eachCourse + " has been selected.")
+                        continue
+                    if (eachCourse in self.enrollCount and
+                            self.enrollCount[eachCourse] == 0):
+                        continue
+                    self.enrollCount[eachCourse] = 1
+                    result = self.__enrollCourse(eachCourse,
+                                                 self.coursesId[eachCourse])
+                    if result:
+                        self.enrollCount[eachCourse] = 0
+
+                for enroll in self.enrollCount:
+                    if self.enrollCount[enroll] == 0:
+                        self.coursesId.pop(enroll)
+                self.enrollCount.clear()
+                if not self.courseId:
+                    return
+        except KeyboardInterrupt:
+            print("Bye")
         except Exception as exception:
             print("System error")
             print(exception)
             exit()
-        except KeyboardInterrupt:
-            print("Bye")
 
     def __enrollCourse(self, courseId, isDegree):
         response = self.s.get(self.courseSelectionBase)
@@ -158,79 +166,6 @@ class UCASEvaluate:
             print("No such course")
             return True
 
-    # def getCourse(self):
-    #     response = self.s.get(self.courseSystem, headers=self.headers)
-    #     soup = BeautifulSoup(response.text, 'html.parser')
-    #     if debug:
-    #         print(response.text.encode('utf8'))
-    #     indentity = str(soup.noscript).split('Identity=')[1].split('"'[0])[0]
-    #     coursePage = self.courseIdentify + indentity
-    #     response = self.s.get(coursePage)
-    #     response = self.s.get(self.courseSelected)
-    #     soup = BeautifulSoup(response.text, 'html.parser')
-    #     listLi = (str)(soup.select('div[class="Menubox"]')[0]).split('SwichClass(this,"MainFrame",')
-    #     evaluateCouse = self.studentCourseEvaluateUrl + listLi[3].split('"')[1]
-    #     response = self.s.get(evaluateCouse)
-    #     soup = BeautifulSoup(response.text, 'html.parser')
-    #     courseListResource = soup.body.table.tbody.find_all('tr')[3:-2]
-    #     courseDict = {}
-    #     if len(courseListResource) == 0:
-    #         self.courseDict = courseDict
-    #     for course in courseListResource:
-    #         tdList = course.find_all('td')
-    #         if tdList[-1].a is None:
-    #             continue
-    #         courseUrl = tdList[-1].a['href']
-    #         courseName = tdList[1].a.string.strip()
-    #         evaluateFlag = tdList[-1].a.string.encode('utf-8')
-    #         if evaluateFlag == '评估':
-    #             courseDict[courseName] = courseUrl
-    #     self.courseDict = courseDict
-
-    # def evaluateCourse(self):
-    #     if len(self.courseDict) == 0:
-    #         print('there is no course need to be evaluated')
-    #         return
-    #     for course in self.courseDict:
-    #         print('start evaluate ' + course + '...')
-    #         evaluateUrl = self.studentCourseEvaluateUrl + 'Evaluate/' + self.courseDict[course]
-    #         self.__evaluate(evaluateUrl)
-
-    # def __evaluate(self, evaluateUrl):
-    #     postData = {}
-    #     response = self.s.get(evaluateUrl)
-    #     soup = BeautifulSoup(response.text, 'html.parser')
-    #     formResource = soup.body.form
-    #     formList = formResource.contents
-
-    #     for inputResource in formResource.contents:
-    #         if (str)(inputResource).strip() != '':
-    #             if inputResource.name == 'input':
-    #                 postData[inputResource['name']] = inputResource['value']
-    #             elif inputResource.name == 'table':
-    #                 subTable = inputResource.contents[1].contents[2].td.table.find_all(attrs={'class': 'GbText'})
-    #                 for eachTable in subTable:
-    #                     for eachInput in eachTable.find_all('input'):
-    #                         label = eachInput.next_sibling.string.encode('utf-8')
-    #                         if label == '优' or label.find('优') != -1:
-    #                             postData[eachInput['name']] = eachInput['value']
-    #                 # subText = inputResource.contents[1].contents[4]
-    #                 postData['rbtnList'] = 5
-    #             postData['tbMerit'] = "本课程老师非常优秀，讲的知识也非常有用。"
-    #             postData['tbSuggest'] = ''
-    #             postData['tbFlaw'] = ''
-    #             postData['btnSave'] = "保存我的评论"
-
-    #     response = self.s.post(evaluateUrl, data=postData)
-    #     if (response
-    #             .text.encode('utf-8')
-    #             .find("<script>alert('恭喜您，提交对该课程的评论成功……')</script>") != -1):
-    #         print('evaluate success...')
-    #     else:
-    #         if debug:
-    #             print(response.text)
-    #         print('evaluate error!')
-
 
 if __name__ == "__main__":
     ucasEvaluate = UCASEvaluate()
@@ -244,12 +179,3 @@ if __name__ == "__main__":
         print('Enrolling start')
         ucasEvaluate.enrollCourses()
         print('Enrolling finish')
-
-    # if ucasEvaluate.evaluate:
-    #     print('Evaluating course...')
-    #     ucasEvaluate.getCourse()
-    #     if len(ucasEvaluate.courseDict) == 0:
-    #         print('There is no course need to be evaluated')
-    #         exit()
-    #     print(str(len(ucasEvaluate.courseDict)) + ' courses need to be evaluated.')
-    #     ucasEvaluate.evaluateCourse()
